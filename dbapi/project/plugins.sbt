1114f1bda3e987619ca54ea9d631ceb4dca97ce4
John Regan <johnreganit@gmail.com>
2019-02-01 12:38:20 +0000

    clean up directory structure

---
+++ dbapi/project/plugins.sbt
@@ -0,0 +1,7 @@
+addSbtPlugin("org.lyranthe.sbt" % "partial-unification" % "1.1.0")
+
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.9.1")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.9")
---
+++ dbapi/src/main/scala/com/johnregan/dbapi/IngestionService.scala
@@ -0,0 +1,51 @@
+package com.johnregan.dbapi
+
+import java.time.ZonedDateTime
+import java.util.UUID
+
+import cats.effect.Sync
+import cats.syntax.all._
+import com.johnregan.dbapi.Ingestion._
+import com.typesafe.scalalogging.LazyLogging
+import io.circe.syntax._
+import org.http4s._
+import org.http4s.circe.CirceEntityDecoder._
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+import JsonHandler._
+import io.circe._
+import io.circe.Json
+import org.http4s
+
+class IngestionService[F[_]: Sync](repository: IngestionRepository[F])
+    extends Http4sDsl[F]
+    with LazyLogging
+    with Http4sInstances {
+
+  val service: HttpService[F] = {
+    HttpService[F] {
+      case req @ PUT -> Root / "ingest" / "language" / language / "source" / source =>
+        req.as[Ingestions].flatMap { is =>
+          val potentailRecords = is.jobEntries.map {
+            case IngestRequest(href, description) =>
+              Ingestion(UUID.randomUUID(), href, description, ZonedDateTime.now(), source, language)
+          }
+
+          repository.insert(potentailRecords).attempt.flatMap {
+            case Right(rowsUpdated) if rowsUpdated > 0 =>
+              Created(s"Created $rowsUpdated new entries in the database".asJson)
+            case Right(_) => NoContent()
+            case Left(error) =>
+              logger.error(s"unexpected error when writing source:$source to the database", error)
+              BadRequest("Error encountered when writing to the database")
+          }
+        }
+      case GET -> Root / "jobs" / "language" / language =>
+        Ok(repository.getIngestions(language).map(_.asJson))
+      case GET -> Root / "languages" =>
+        repository.getLanguages.flatMap { ls =>
+          Ok(ls.asJson)
+        }
+    }
+  }
+}
